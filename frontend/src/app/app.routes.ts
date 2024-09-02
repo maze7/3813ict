29d97537249480aa7053eea0e8a959260719c427
callan <callan@mosaik.au>
2024-09-02 19:54:29 +1000

    add skeleton for register component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,16 @@ import {LoginComponent} from "./pages/login/login.component";
 import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 import {ChatComponent} from "./pages/chat/chat.component";
 import {ProfileComponent} from "./pages/profile/profile.component";
+import {RegisterComponent} from "./pages/register/register.component";
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
   {
     path: 'dashboard',
---
+++ frontend/src/app/pages/register/register.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {LucideAngularModule} from "lucide-angular";
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    LucideAngularModule
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}
