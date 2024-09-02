186ba12f7dac4ba3cb922f0cac59da8ad67cd331
callan <callan@mosaik.au>
2024-09-02 18:19:04 +1000

    add lucide icons, update colors, polish login screen

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { LucideAngularModule, ShieldX } from 'lucide-angular';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(LucideAngularModule.pick({ ShieldX })),
+  ]
 };
--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {LucideAngularModule} from 'lucide-angular';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    LucideAngularModule,
+    NgClass
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  public authError: boolean = false;
 }
