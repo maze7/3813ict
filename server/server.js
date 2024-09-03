a9dfee59d3642c8d544c36ac78e200d10ee325ac
callan <callan@mosaik.au>
2024-09-03 21:32:59 +1000

    add jwt middleware and auth routes to backend

--- server/server.js
+++ server/server.js
@@ -32,24 +32,6 @@ app.post('/login', async (req, res) => {
     res.json({ token });
 });
 
-// Protected Route
-app.get('/protected', authenticateToken, (req, res) => {
-    res.json({ message: `Hello, ${req.user.username}! You are authenticated.` });
-});
-
-// Middleware to Authenticate Token
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (!token) return res.sendStatus(401);
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-}
-
 // Start Server
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
