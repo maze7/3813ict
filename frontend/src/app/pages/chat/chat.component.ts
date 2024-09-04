01140d23d5ae4f1db81c60e6aebae53446518b81
callan <callan@mosaik.au>
2024-09-05 00:28:58 +1000

    style changes

--- frontend/src/app/pages/chat/chat.component.ts
+++ frontend/src/app/pages/chat/chat.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {LucideAngularModule} from "lucide-angular";
 import {GroupService} from "../../services/group.service";
 import {AuthService} from "../../services/auth.service";
-import {AsyncPipe} from "@angular/common";
+import {AsyncPipe, NgClass} from "@angular/common";
 import {NewChannelModalComponent} from "../../components/new-channel-modal/new-channel-modal.component";
 
 @Component({
@@ -11,11 +11,22 @@ import {NewChannelModalComponent} from "../../components/new-channel-modal/new-c
   imports: [
     LucideAngularModule,
     AsyncPipe,
-    NewChannelModalComponent
+    NewChannelModalComponent,
+    NgClass
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
+
+  protected messages = [
+    { name: 'Ronald', img: 'ronald.png', time: '12:45', msg: 'I feel like noodles!' },
+    { name: 'Frank', img: 'frank.png', time: '12:45', msg: 'What type of noodles?'},
+    { name: 'Sally', img: 'sleepysally.png', time: '12:46', msg: 'ramen!' },
+    { name: 'Ronald', img: 'ronald.png', time: '12:45', msg: 'They get stuck in my beard..!' },
+    { name: 'Ben', img: 'ben.png', time: '12:32', msg: `That's how you save them for later.` },
+    { name: 'Sally', img: 'sleepysally.png', time: '12:46', msg: 'infinite noodles!' },
+  ]
+
   constructor(protected groups: GroupService, protected auth: AuthService) {}
 }
